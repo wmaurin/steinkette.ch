51350ea41de5c016235c0024b33a60d87d195b0e
wmaurin <dev.maurin@gmail.com>
2026-01-30 20:10:57 +0100

    fix: base path for github pages

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/steinkette.ch">
       <App />
     </BrowserRouter>
   </StrictMode>
